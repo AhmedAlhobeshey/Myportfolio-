5dd13db1859f37907d1439f5d380dcde5ad71786
AhmedAlhobeshey <95928823+AhmedAlhobeshey@users.noreply.github.com>
2024-11-09 01:23:09 +0300

    Update Education.js

--- src/containers/education/Education.js
+++ src/containers/education/Education.js
@@ -7,7 +7,7 @@ export default function Education() {
   if (educationInfo.display) {
     return (
       <div className="education-section" id="education">
-        <h1 className="education-heading">مسيرتي الدراسية</h1>
+        <h1 className="education-heading">مسيرتي التعليمية</h1>
         <div className="education-card-container">
           {educationInfo.schools.map((school, index) => (
             <EducationCard key={index} school={school} />
