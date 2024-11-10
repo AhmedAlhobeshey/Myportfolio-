0358f2e7029d25bb5adb398e7a17302790910c28
AhmedAlhobeshey <95928823+AhmedAlhobeshey@users.noreply.github.com>
2024-11-11 00:18:00 +0300

    Update WorkExperience.js

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -1,48 +1,42 @@
-import React, { useContext, useEffect, lazy, Suspense } from "react";
+import React, {useContext} from "react";
 import "./WorkExperience.scss";
-const ExperienceCard = lazy(() => import("../../components/experienceCard/ExperienceCard"));
-import { workExperiences } from "../../portfolio";
-import { Fade } from "react-reveal";
+import ExperienceCard from "../../components/experienceCard/ExperienceCard";
+import {workExperiences} from "../../portfolio";
+import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function WorkExperience() {
-  const { isDark } = useContext(StyleContext);
-
-  useEffect(() => {
-    console.log(workExperiences.experience);
-  }, []);
-
-  if (!workExperiences.display) {
-    return null;
-  }
-
-  return (
-    <div id="experience">
-      <Fade bottom duration={1000} distance="20px">
-        <div className="experience-container" id="workExperience">
-          <div>
-            <h1 className="experience-heading">الخبرات المهنية</h1>
-            <div className="experience-cards-div">
-              <Suspense fallback={<div>Loading...</div>}>
-                {workExperiences.experience.map((card, i) => (
-                  <ExperienceCard
-                    key={i}
-                    isDark={isDark}
-                    cardInfo={{
-                      company: card.company,
-                      desc: card.desc,
-                      date: card.date,
-                      companylogo: card.companylogo,
-                      role: card.role,
-                      descBullets: card.descBullets
-                    }}
-                  />
-                ))}
-              </Suspense>
+  const {isDark} = useContext(StyleContext);
+  if (workExperiences.display) {
+    return (
+      <div id="experience">
+        <Fade bottom duration={1000} distance="20px">
+          <div className="experience-container" id="workExperience">
+            <div>
+              <h1 className="experience-heading">الخبرات المهنية </h1>
+              <div className="experience-cards-div">
+                {workExperiences.experience.map((card, i) => {
+                  return (
+                    <ExperienceCard
+                      key={i}
+                      isDark={isDark}
+                      cardInfo={{
+                        company: card.company,
+                        desc: card.desc,
+                        date: card.date,
+                        companylogo: card.companylogo,
+                        role: card.role,
+                        descBullets: card.descBullets
+                      }}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
-        </div>
-      </Fade>
-    </div>
-  );
-    }
+        </Fade>
+      </div>
+    );
+  }
+  return null;
+}
